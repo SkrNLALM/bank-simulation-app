1ce97866e5d25ed2d13ba35018c08130090b2511
SkrNLALM <cakmakskr@gmail.com>
2023-03-18 21:39:02 +0100

    HTML files added

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -1,13 +1,39 @@
 package com.cydeo;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
+import com.cydeo.repository.TransactionRepository;
+import com.cydeo.services.AccountService;
+import com.cydeo.services.TransactionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @SpringBootApplication
 public class BankSimulationAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BankSimulationAppApplication.class, args);
+
+        ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
+
+
+//        //get account and transaction service beans
+//      AccountService accountService=container.getBean(AccountService.class);
+//      TransactionService transactionService=container.getBean(TransactionService.class);
+//
+//      //create 2 account sender and receiver
+//        Account sender=accountService.createNewAccount(BigDecimal.valueOf(70), new Date(), AccountType.CHECKING, 1L);
+//        Account receiver=accountService.createNewAccount(BigDecimal.valueOf(50), new Date(), AccountType.SAVING, 1L);
+//        accountService.listAllAccounts().forEach(System.out::println);
+//
+//        transactionService.makeTransfer(sender,receiver, new BigDecimal(70), new Date(), "Transaction 1");
+//        System.out.println(transactionService.findAllTransaction().get(0));
+
+
     }
 
 }
--- src/main/java/com/cydeo/services/impl/TransactionServiceimpl.java
+++ src/main/java/com/cydeo/services/impl/TransactionServiceimpl.java
@@ -17,12 +17,12 @@ import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
+
 @Component
 public class TransactionServiceimpl implements TransactionService {
 
-    @Value("{$under_Construction}")
-    private  boolean underConstruction;
-
+    @Value("${under_construction}")
+    private boolean underConstruction;
 
     private final AccountRepository accountRepository;
     private final TransactionRepository transactionRepository;
@@ -32,89 +32,87 @@ public class TransactionServiceimpl implements TransactionService {
         this.transactionRepository = transactionRepository;
     }
 
-       @Override
-       public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
-       /*
-       -if the sender or receiver is null?
-       -if sender and receiver is the same account?
-       -if sender has enough balance ?
-       -if both account are checking, if not one of them saving, it needs to be same userID
+    @Override
+    public Transaction makeTransfer(Account sender, Account receiver, BigDecimal amount, Date creationDate, String message) {
 
-       */
-           if(!underConstruction){
-       validateAccount(sender, receiver);
-       checkAccountOwnership(sender, receiver);
-       executeBalanceAndUpdateIsRequired(amount,sender,receiver);
+        if (!underConstruction) {
+            validateAccount(sender,receiver);
+            checkAccountOwnership(sender,receiver);
+            executeBalanceAndUpdateIfRequired(amount,sender,receiver);
+        /*
+            after all validations are completed, and money is transferred, we need to  create Transaction object and save/return it.
+         */
 
-    /*
-    * after all validations are completed, and money is transferred, we need to create Transaction object and save/return it
-    *
-    *
-    *
-   */
+            Transaction transaction = Transaction.builder().amount(amount).sender(sender.getId())
+                    .receiver(receiver.getId()).creationDate(creationDate).message(message).build();
 
-           Transaction transaction=Transaction.builder().amount(amount).sender(sender.getId())
-                   .receiver(receiver.getId()).creationDate(creationDate).message(message).build();
+            return transactionRepository.save(transaction);
 
-       return transactionRepository.save(transaction);
-    }else {
-           throw new UnderConstructionException("App is under construction, try again later");
-           }
-       }
+        }else {
+            throw new UnderConstructionException("App is under construction,try again later.");
+        }
 
-    private void executeBalanceAndUpdateIsRequired(BigDecimal amount, Account sender, Account receiver) {
+
+
+    }
+
+    private void executeBalanceAndUpdateIfRequired(BigDecimal amount, Account sender, Account receiver) {
         if(checkSenderBalance(sender,amount)){
-            //make balance transfer between sender and receiver
+            //update sender and receiver balance
             sender.setBalance(sender.getBalance().subtract(amount));
             receiver.setBalance(receiver.getBalance().add(amount));
 
-          }else{
-            //throw BalanceNotSufficientException
+        }else{
             throw new BalanceNotSufficientException("Balance is not enough for this transfer.");
-
         }
+
     }
 
     private boolean checkSenderBalance(Account sender, BigDecimal amount) {
         //verify sender has enough balance to send
-
-        return sender.getBalance().subtract(amount).compareTo(BigDecimal.ZERO)>=0;
+        return sender.getBalance().subtract(amount).compareTo(BigDecimal.ZERO) >=0;
     }
 
-    private void checkAccountOwnership(Account sender, Account receiver)  {
-        //write an if statement that checks if one of the account is saving
-        // and user or receiver is not the same, throw AccountOwnershipException
+    private void checkAccountOwnership(Account sender, Account receiver) {
+        /*
+            write an if statement that checks if one of the account is saving,
+            and user of sender or receiver is not the same, throw AccountOwnershipException
+         */
 
         if((sender.getAccountType().equals(AccountType.SAVING)||receiver.getAccountType().equals(AccountType.SAVING))
-            &&!sender.getUserId().equals(receiver.getUserId())){
-        throw new AccountOwnershipException ("Since you are using a saving account, the sender and receiveruserId must be the same");
-
+                && !sender.getUserId().equals(receiver.getUserId())){
+            throw new AccountOwnershipException("Since you are using a savings account, the sender and receiver userId must be the same.");
         }
+
     }
 
     private void validateAccount(Account sender, Account receiver) {
-       /*
+        /*
             -if any of the account is null
             -if account ids are the same(same account)
             -if the accounts exist in the database(repository)
          */
 
-       if(sender==null||receiver==null){
+        if(sender==null||receiver==null){
             throw new BadRequestException("Sender or Receiver cannot be null");
         }
-       if(sender.getId().equals(receiver.getId())){
+
+        if(sender.getId().equals(receiver.getId())){
             throw new BadRequestException("Sender account needs to be different than receiver");
         }
-       //verify if we have sender and receiver in the database
+        //verify if we have sender and receiver in the database
         findAccountById(sender.getId());
         findAccountById(receiver.getId());
-        }
-       private void findAccountById(UUID id) {
-        accountRepository.findById(id);
-        }
-       @Override
-       public List<Transaction> findAllTransaction() {
 
-       return transactionRepository.findAll();
+
+    }
+
+    private void findAccountById(UUID id) {
+        accountRepository.findById(id);
+    }
+
+    @Override
+    public List<Transaction> findAllTransaction() {
+        return transactionRepository.findAll();
     }
 }
